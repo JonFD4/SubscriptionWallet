bfb5e7125ed22c768a22cca0b379c7324562647e
Jonathan Forson <jonathanforsondanso@gmail.com>
2024-05-07 19:34:05 +0000

    import and register models

--- subscription_wallet_app/admin.py
+++ subscription_wallet_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Category, User, Subscription, Payment
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(User)
+admin.site.register(Subscription)
+admin.site.register(Payment)
