835d553b89a5bfd8d078678a5a65d49b4b75137a
Jonathan Forson <jonathanforsondanso@gmail.com>
2024-05-08 20:51:06 +0000

    Set template directory

--- subscriptionwallet/settings.py
+++ subscriptionwallet/settings.py
@@ -63,10 +63,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'subscriptionwallet.urls'
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['TEMPLATES_DIR'],
+        #'DIRS': ['TEMPLATES_DIR'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
