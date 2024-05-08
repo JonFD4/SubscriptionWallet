bc3d056e7db5b99e5a320a74998265cb47f53236
Jonathan Forson <jonathanforsondanso@gmail.com>
2024-05-08 20:50:16 +0000

    Import view and add indexview to path

--- subscriptionwallet/urls.py
+++ subscriptionwallet/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from subscription_wallet_app.views import IndexView
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
+    path('', IndexView.as_view(), name='home'),
+    path('summernote/', include('django_summernote.urls')),
 ]
