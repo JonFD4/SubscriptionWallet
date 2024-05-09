609c0034b4e703edd92265bff35f3c0bc241d8f9
Jonathan Forson <jonathanforsondanso@gmail.com>
2024-05-09 16:12:08 +0000

    Add functions and class to render index, subscription list and subscription details

--- subscription_wallet_app/views.py
+++ subscription_wallet_app/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
+from django.views import generic
+from .models import Subscription
 
 # Create your views here.
 
-class IndexView(TemplateView):
+class index_view(TemplateView):
     template_name = 'index.html'
 
+@login_required
+def subscription_list(request):
+    # Get subscriptions for the current user
+    subscriptions = Subscription.objects.filter(user__django_user=request.user)
+
+    context = {
+        'subscriptions': subscriptions
+    }
+    return render(request, 'subscription_list.html', context)
+
+@login_required
+def subscription_detail(request, subscription_id):
+    # Get the subscription for the current user
+    subscription = get_object_or_404(Subscription, id=subscription_id, user__django_user=request.user)
+
+    context = {
+        'subscription': subscription
+    }
+    return render(request, 'subscription_detail.html', context)
+
+# You can add more views here for creating, updating, and deleting subscriptions if needed
