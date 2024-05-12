93d89a1d39de489bf739bf464d07069ed6d0b71c
Jonathan Forson <jonathanforsondanso@gmail.com>
2024-05-12 19:48:18 +0000

    Create user instance

--- subscription_wallet_app/views.py
+++ subscription_wallet_app/views.py
@@ -1,13 +1,44 @@
 from django.shortcuts import render,redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User 
 from django.views.generic import TemplateView
 from django.views import generic
 from .models import Subscription
+from .models import user
+from .forms import YourSignUpForm
+
 
 # Create your views here.
 
 class index_view(TemplateView):
     template_name = 'index.html'
+
+# Creating user instance and adding user to database
+def signup(request):
+    if request.method=='POST':
+        form = YourSignUpForm(request.post)
+        if form is_valid():
+            username= form.cleaned_data('username')
+
+
+
+@login_required
+def dashboard(request):
+    #Get user subscriptions
+    user = request.user 
+    user_subscriptions = Subscription.objects.filter(user=user)
+
+    subscription_data =[]
+    for subscription in user_subscriptions:
+        subscriptions_info.append({
+            'name': subscription.name,
+            'next_payment_date': subscription.next_payment_date
+        })
+    context = {
+        'subscriptions_info': subscriptions_info
+    }
+    return render(request, 'dashboard.html', context)
+
 
 @login_required
 def subscription_list(request):
@@ -28,5 +59,3 @@ def subscription_detail(request, subscription_id):
         'subscription': subscription
     }
     return render(request, 'subscription_detail.html', context)
-
-# You can add more views here for creating, updating, and deleting subscriptions if needed
